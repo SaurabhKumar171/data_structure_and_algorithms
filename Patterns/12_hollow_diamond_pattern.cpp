282258bcdb1b563d562616b2dad44aea55e0d92a
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-05-14 09:27:15 +0000

    HollowDiamond Pattern

--- Patterns/12_hollow_diamond_pattern.cpp
+++ Patterns/12_hollow_diamond_pattern.cpp
@@ -35,6 +35,28 @@ int hollow_diamond_pattern(){
     }
 
     cout << "\n";
-  }  
+  }
+
+  for(int i=1; i<= n; i++){
+
+    for(int j=0; j <=i; j++){
+      cout << " ";
+    }
+
+    for(int k=0; k <= 2*(n-i)-1; k++){
+      if(i == n){
+        cout << "*";
+      }
+      else{
+        if(k==0 || k == 2*(n-i)-2){
+           cout << "*";
+        }
+        else{
+          cout << " ";
+        }
+      }
+    }
+    cout << endl;
+  }
   return 0;
 }
