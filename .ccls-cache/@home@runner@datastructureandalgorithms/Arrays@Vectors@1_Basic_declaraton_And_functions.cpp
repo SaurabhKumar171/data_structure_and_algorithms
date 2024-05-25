9ddea133a3803fc07a048a17e32088ef9dba3bf0
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-05-25 14:30:05 +0000

    Unique element in an array

---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@Vectors@1_Basic_declaraton_And_functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int vector_basics(){
+  vector<int> v;
+
+  cout <<"Vector size is : "<< v.size()<< endl;
+
+  cout <<"Vector capacity is : "<< v.capacity()<< endl;
+
+  return 0;
+}
--- .ccls-cache/@home@runner@datastructureandalgorithms/main.cpp
+++ .ccls-cache/@home@runner@datastructureandalgorithms/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int extreme_array(int arr[], int size);
+// int extreme_array(int arr[], int size);
+
+// int main() {
+//       int arr[] = {50, 80, 30, 10, 60};
+//       int size = sizeof(arr) / sizeof(arr[0]);
+
+//       extreme_array(arr, size);
+//       return 0;
+// } 
+
+int uniqueElement(vector<int> nums);
 
 int main() {
-      int arr[] = {50, 80, 30, 10, 60};
-      int size = sizeof(arr) / sizeof(arr[0]);
+    int n;
 
-      extreme_array(arr, size);
-      return 0;
-} 
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    cout << "Enter array elements: ";
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Unique element: " << uniqueElement(arr) << endl;
+    return 0;
+}
