e9f9c8c11309589769b6223c966d3e2adaf5b92f
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-06-02 06:49:30 +0000

    54. Leetcode (Spiral matrix)

--- .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@2D_Arrays@6_Spiral_matrix.cpp
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@2D_Arrays@6_Spiral_matrix.cpp
@@ -15,24 +15,28 @@ vector<int> spiralOrder(vector<vector<int>>& matrix) {
 
     while(top <= bottom && left <= right){
 
+        // Print starting row
         for(int i = left; i <= right; i++){
             res.push_back(matrix[top][i]);
         }
         top++;
 
+        // Print ending col
         for(int i = top; i <= bottom; i++){
             res.push_back(matrix[i][right]);
         }
         right--;
 
-        if(top <= bottom){
+        // Print ending row
+        if(top <= bottom){ // Check if there is a bottom row to traverse
             for(int i = right; i >= left; i--){
                 res.push_back(matrix[bottom][i]);
             }
             bottom--;
         }
 
-        if(left <= right){
+        // Print starting col
+        if(left <= right){ // Check if there is a left column to traverse
             for(int i = bottom; i >= top; i--){
                 res.push_back(matrix[i][left]);
             }
