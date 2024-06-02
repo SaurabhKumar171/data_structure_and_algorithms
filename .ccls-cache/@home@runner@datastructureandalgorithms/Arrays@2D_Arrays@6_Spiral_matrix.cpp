28098e9f953f64160d3f41943371ca6a0a9b5e19
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-06-02 06:41:39 +0000

    Spiral matrix

---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@2D_Arrays@6_Spiral_matrix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+    if (matrix.empty()) return {};
+
+    int top = 0;
+    int left = 0;
+    int bottom = matrix.size() - 1;
+    int right = matrix[0].size() - 1;
+
+    vector<int> res;
+
+    while(top <= bottom && left <= right){
+
+        for(int i = left; i <= right; i++){
+            res.push_back(matrix[top][i]);
+        }
+        top++;
+
+        for(int i = top; i <= bottom; i++){
+            res.push_back(matrix[i][right]);
+        }
+        right--;
+
+        if(top <= bottom){
+            for(int i = right; i >= left; i--){
+                res.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if(left <= right){
+            for(int i = bottom; i >= top; i--){
+                res.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return res;
+}
