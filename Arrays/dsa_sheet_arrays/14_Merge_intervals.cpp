32bd04387e6a2aed5157e986ac20ae29e86a7238
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-06-07 16:37:27 +0000

    56. Leetcode (Merge intervals) : Brute force

---
+++ Arrays/dsa_sheet_arrays/14_Merge_intervals.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+    int row = intervals.size();
+    int col = intervals[0].size();
+
+    if(intervals.size() <= 1){
+        return intervals;
+    }
+
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> res;
+    vector<bool> mergedFlags(intervals.size(), false);
+
+    for(int i = 0; i < intervals.size() ; i++){
+
+        if (mergedFlags[i]) continue;
+
+        int first = intervals[i][0];
+        int second = intervals[i][1];
+
+        for(int j = i+1; j < intervals.size(); j++){                
+            if(second >= intervals[j][0]){
+                second = max(second, intervals[j][1]);
+
+                mergedFlags[j] = true;
+            }
+        } 
+        res.push_back({first, second});      
+    }
+
+    return res;
+}
