238971fbbc76442994f23f0ce4c1b558104f52de
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-06-08 18:13:55 +0000

    56. Leetode (Merge intervals) : Optimal solution

--- Arrays/dsa_sheet_arrays/14_Merge_intervals.cpp
+++ Arrays/dsa_sheet_arrays/14_Merge_intervals.cpp
@@ -38,3 +38,37 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
  
     return res;
 }
+
+// Efficient solution
+vector<vector<int>> mergeEfficientSolution(vector<vector<int>>& intervals) {
+
+    int row = intervals.size();
+    int col = intervals[0].size();
+
+    if(row == 1){
+        return intervals;
+    }
+
+    vector<vector<int>> res;
+
+    sort(intervals.begin(), intervals.end());
+
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+
+    for(int i = 1; i < row ; i++){
+
+       if(intervals[i][0] <= end){
+         end = max(end, intervals[i][1]);
+       }
+       else{
+        res.push_back({start, end});
+        start = intervals[i][0];
+        end = intervals[i][1];
+       }
+
+    }
+    res.push_back({start, end});
+
+    return res;
+}
