9c0c939f83a95d51f362689f52745a1b1fdc43dd
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-07-10 18:28:26 +0000

    Sorted matrix

---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@DSA_sheet@Matrix@5_Sorted_matrix.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+    // code here
+    vector<int> store;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            store.push_back(Mat[i][j]);
+        }
+    }
+
+    sort(store.begin(), store.end());
+
+    int k = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            Mat[i][j] = store[k++];
+        }
+    }
+
+    return Mat;
+}
