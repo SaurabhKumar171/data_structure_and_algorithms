1edf752182cff66206d0425e45fc8a4190d59516
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-07-11 19:39:37 +0000

    Find a specific pair in Matrix

---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@2D_Arrays@3_Max_and_min_elements_in_matrix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int min(int arr[][3],int n ,int m,int target){
+  int min = INT_MAX;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+        if(arr[i][j] < min){
+          min = arr[i][j];
+        }
+    }
+  }
+
+  return min;
+}
+
+int max(int arr[][3],int n ,int m,int target){
+
+  int max = INT_MIN;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+        if(arr[i][j] > max){
+          max = arr[i][j];
+        }
+    }
+  }
+  return max;
+}
+
+// int main() {
+//   // declare 2D Array
+//   int arr[3][3];
+
+//   // int brr[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+//   for(int i = 0; i < 3; i++){
+//     for(int j = 0; j < 3; j++){
+//         cin >> arr[i][j];
+//     }
+//   }
+
+//   cout << "Max is : " << max(arr, 3 , 3, 0) << endl;
+
+//   cout << "Min is : " << min(arr, 3 , 3, 0) << endl;
+
+//   return 0;
+// }
--- .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@DSA_sheet@Matrix@6_Find_a_specific_pair_in_Matrix.cpp
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@DSA_sheet@Matrix@6_Find_a_specific_pair_in_Matrix.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <iostream>
 #include <limits.h>
+#include <vector>
 using namespace std;
 
-// Brute force (o(n^2)))
+// Brute force (o(n^4)))
 int specificPair1(int mat[][5], int n) {
 
   int maxDiff = INT_MIN;
@@ -21,4 +23,39 @@ int specificPair1(int mat[][5], int n) {
   }
 
   return maxDiff;
+}
+
+// Optimal approach (o(n^2))
+int specificPair(int mat[][5], int n){
+
+    vector<vector<int>> maxMat(n, vector<int>(n));
+
+    // store last element of matrix in new matMax
+    maxMat[n-1][n-1] = mat[n-1][n-1];
+
+    // store last row of matrix in new matMax
+    for(int j = n-2; j >= 0; j--){
+        maxMat[n-1][j] = max(maxMat[n-1][j+1], mat[n-1][j]);
+    }
+
+    // store last col of matrix in new matMax
+    for(int i = n-2; i >= 0; i--){
+        maxMat[i][n-1] = max(maxMat[i+1][n-1], mat[i][n-1]);
+    }
+
+    // store rest elements in new matMax
+    for(int i = n-2; i >= 0; i--){
+      for(int j = n-2; j >= 0; j--){
+        maxMat[i][j] = max({maxMat[i+1][j], maxMat[i][j+1], mat[i][j]});
+      }
+    }
+
+    int ans = INT_MIN;
+    for(int i = 0; i < n-1; i++){
+      for(int j = 0; j < n-1; j++){
+        ans = max(ans, (maxMat[i+1][j+1] - mat[i][j]));
+      }
+    }
+
+    return ans;
 }
