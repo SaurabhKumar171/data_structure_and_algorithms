51b3d92feaae8ad892da657b557d56ebf6e095c5
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-07-13 18:01:16 +0000

    Palindrome String

---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@DSA_sheet@Matrix@9_Common_elements_in_all_rows_of_a_given_matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+void commonElements(vector<vector<int>> arr, int n, int m){
+  unordered_map<int, int> map;
+
+  for(int i = 0; i < arr[0].size(); i++){
+    map[arr[0][i]]++;
+  }
+
+  for(int i = 1; i < n; i++){
+    for(int j = 0; j < m; j++){
+        if(map.find(arr[i][j]) != map.end() && map[arr[i][j]] == i){
+          map[arr[i][j]]++;
+        }
+    }
+  }
+
+
+  // Traverse the map
+    for (auto it = map.begin(); it != map.end(); ++it) {
+        if (it->second == n) {
+            cout << it->first << " ";
+        }
+    }
+    cout << endl;
+}
---
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@DSA_sheet@Strings@Palindrome_String.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int isPalindrome(string S) {
+  // Your code goes here
+  int i = 0;
+  int j = S.size() - 1;
+
+  while (i < j) {
+    if (S[i] != S[j]) {
+      return 0;
+    }
+    i++;
+    j--;
+  }
+  return 1;
+}
