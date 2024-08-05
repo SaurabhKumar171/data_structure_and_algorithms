0b3618acfb4e7b9be1f1a118b2e404a6961b6287
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-08-05 17:21:06 +0000

    changes

--- .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@Searching and Sorting@8_Search_in_a_nearly_sorted_array.cpp
+++ .ccls-cache/@home@runner@datastructureandalgorithms/Arrays@Searching and Sorting@8_Search_in_a_nearly_sorted_array.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int searchInNearlySortedArray(int arr[], int n, int target){
-    int i = 0;
-    int j = n - 1;
+int searchInNearlySortedArray(int arr[], int n, int target) {
+  int i = 0;
+  int j = n - 1;
 
-    while(i <= j){
-      int m = i + (j - i)/2;
+  while (i <= j) {
+    int m = i + (j - i) / 2;
 
-      if(arr[m] == target){
-          return m;
-      }
-      else if(arr[m+1] == target){
-          return m+1;
-      }
-      else if(arr[m-1] == target){
-          return m-1;
-      }
-      else if(arr[m] < target){
-          i = m + 2;
-      }
-      else{
-          j = m - 2;
-      }
+    if (arr[m] == target) {
+      return m;
+    } else if (m + 1 < n && arr[m + 1] == target) {
+      return m + 1;
+    } else if (m - 1 >= 0 && arr[m - 1] == target) {
+      return m - 1;
+    } else if (arr[m] < target) {
+      i = m + 2;
+    } else {
+      j = m - 2;
     }
+  }
 
-    return -1;
+  return -1;
 }
