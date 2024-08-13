ad6d2b03fbc6d0374ff9270c04bf38ae81c99fab
Saurabh Kumar <72852126+SaurabhKumar171@users.noreply.github.com>
2024-08-13 09:21:23 +0000

    Missing And Repeating

--- Arrays/Searching and Sorting/17_Find_missing_and_repeating_element.cpp
+++ Arrays/Searching and Sorting/17_Find_missing_and_repeating_element.cpp
@@ -28,3 +28,30 @@ vector<int> findTwoElementBruteForce(vector<int> arr, int n) {
 
   return vector<int>{repeatingNum, missingNum};
 }
+
+// Better Approach
+vector<int> findTwoElementBetterApproach(vector<int> arr, int n) {
+  // code here
+  // int missingNum = missingNumber(arr, n);
+  // int repeatingNum = repeatingNumber(arr, n);
+
+  int repeatingNum = -1;
+  int missingNum = -1;
+
+  unordered_map<int, int> map;
+
+  for (int i = 0; i < n; i++) {
+    map[arr[i]]++;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    if (map[i] == 0) {
+      missingNum = i; // Number i is missing
+    }
+    if (map[i] > 1) {
+      repeatingNum = i; // Number i is repeating
+    }
+  }
+
+  return vector<int>{repeatingNum, missingNum};
+}
