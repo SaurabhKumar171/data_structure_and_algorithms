195454126409a239098d77d5fcbcfbc8c1c1df48
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-08-28 01:09:27 +0530

    create Linked List using array

---
+++ Linked Lists/Basics of Linked Lists/3_Traverse_in_Linked_list.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node {
+  public:
+  int data;
+  Node* next;
+
+  public:
+  Node(int data1, Node* next1){
+    data = data1;
+    next = next1;
+  }
+
+  public:
+  Node(int data1){
+    data = data1;
+    next = nullptr;
+  }
+};
+
+Node* convertArrayToLinkedList(vector<int> arr){
+
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1; i < arr.size(); i++){
+        Node* newNode = new Node(arr[i]);
+        mover->next = newNode;
+
+        mover = newNode;
+    }
+
+    return head; 
+}
+
+int main() {
+  vector<int> arr = {6, 5, 7 ,9};
+
+  Node* head = convertArrayToLinkedList(arr);
+
+  Node* temp = head;
+
+  while(temp){
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+}
