e0c811734e72027caea096a7e523c29a0ce39e3e
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-02 16:38:35 +0530

    Array to DLL

--- Linked Lists/Basics of Linked Lists/14_Convert_Array_to_DLL.cpp
+++ Linked Lists/Basics of Linked Lists/14_Convert_Array_to_DLL.cpp
@@ -1,16 +1,59 @@
-ListNode* arrayToLinkedList(vector<int> &nums) {
-    if(nums.size() == 0){
-        return NULL;
+#include <iostream>
+using namespace std;
+
+
+class Node{
+  public:
+    int data;
+    Node* next;
+    Node* back;
+
+  public:
+    Node(int data1, Node* next1, Node* back1){
+      data = data1;
+      next = next1;
+      back = back1;
     }
-    ListNode* head = new  ListNode(nums[0]);
-    ListNode* temp = head;
-    if(nums.size() == 1){
-        return head;
+
+  public:
+    Node(int data1){
+      data = data1;
+      next = nullptr;
+      back = nullptr;
     }
-    for(int i = 1; i < nums.size(); i++){
-        ListNode* newNode = new ListNode(nums[i], nullptr, temp);
-        temp->next = newNode;
-        temp = temp->next;
-    }
-    return head;
+};
+
+Node* converArrToDll(int arr[5], int n){
+
+  Node* head = new Node(arr[0]);
+  Node* current = head;
+
+  for(int i = 1; i < n; i++){
+    Node* newNode = new Node(arr[i]);
+    newNode->back = current;
+    current->next = newNode;
+
+    current = newNode;
+  }
+
+  return head;
+}
+
+// Function to print the Doubly Linked List
+void printDll(Node* head) {
+  Node* current = head;
+  while (current != nullptr) {
+    cout << current->data << " ";
+    current = current->next;
+  }
+  cout << endl;
+}
+
+int main() {
+  int arr[5] = {5, 3, 8, 9, 0};
+
+  Node* head = converArrToDll(arr, 5);
+  printDll(head);
+
+  return 0;
 }
