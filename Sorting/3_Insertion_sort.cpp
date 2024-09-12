eeb4d24996260de43dc5feffc0526b3ee6f318bc
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-12 23:41:33 +0530

    Sorting : Insertion sort - solution

---
+++ Sorting/3_Insertion_sort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> insertionSort(vector<int>& nums) {
+
+        for(int i = 0; i < nums.size(); i++){
+
+            int j = i;
+
+            while(j > 0 && nums[j-1] > nums[j]){
+                swap(nums[j-1], nums[j]);
+                j--;
+            }
+        }
+
+        return nums;
+    }
+};
