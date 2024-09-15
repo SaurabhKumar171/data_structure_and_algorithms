a2ae89f38558de31abe8197cb6344fb84662bcec
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-15 15:55:15 +0530

    Arrays : Move zeroes to end of array - Optimal Approach

--- Arrays/tuf/2_Move_zeroes_to_end.cpp
+++ Arrays/tuf/2_Move_zeroes_to_end.cpp
@@ -1,5 +1,5 @@
 // Brute force
-class Solution {
+class Solution1 {
 public:
     void moveZeroesBruteForce(vector<int>& nums) {
 
@@ -15,6 +15,38 @@ public:
 
         for(int i = 0; i < temp.size(); i++){
             nums[i] = temp[i];
+        }
+    }
+};
+
+// Optimal approach
+class Solution2 {
+public:
+    void moveZeroesOptimalApproach(vector<int>& nums) {
+
+        int n = nums.size();
+        int i = -1;
+        int j = -1;
+
+        for(int k = 0; k < n; k++){
+            if(nums[k] == 0){ 
+                i = k;
+                break;
+            }
+        }
+
+        if(i == -1) return;
+
+        j = i + 1;
+
+        while(i < n && j < n){
+            if(nums[i] != 0) i++;
+            if(nums[j] == 0) j++;
+            else if(nums[i] == 0 && nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
         }
     }
 };
