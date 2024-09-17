0333a9135ed2bab9dcaaac7561c77faaefd1e6b3
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-17 20:29:38 +0530

    Arrays : Remove duplicates from sorted array - Optimal Approach

--- Arrays/tuf/3_Remove_duplicates_from_sorted_array.cpp
+++ Arrays/tuf/3_Remove_duplicates_from_sorted_array.cpp
@@ -19,3 +19,32 @@ public:
         return k;
     }
 };
+
+// Optimal approach
+class Solution2 {
+public:
+    int removeDuplicatesOptimalApproach(vector<int>& nums) {
+        
+        int n = nums.size();
+        int i = 0;
+        // int j = 1;
+
+
+        for (int j = 1; j < nums.size(); j++) {
+            /*If current element is different 
+            from the previous unique element*/
+            if (nums[i] != nums[j]) {
+                /* Move to the next position in 
+                the vector for the unique element*/
+                i++;
+                /* Update the current position 
+                with the unique element*/
+                swap(nums[i], nums[j]);
+            }
+        }
+
+        int k = i + 1;
+
+        return k;
+    }
+};
