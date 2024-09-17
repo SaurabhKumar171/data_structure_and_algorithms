7c1d3a7e2e143cb7329fd5dbc3ae8633a2988a5b
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-17 20:54:59 +0530

    Arrays : Find missing number in an array - Optimal approach

--- Arrays/tuf/4_Find_missing_number_in_array.cpp
+++ Arrays/tuf/4_Find_missing_number_in_array.cpp
@@ -15,3 +15,25 @@ public:
         return sumOfNNumbers - totalSum;
     }
 };
+
+// Optimal Approach - I
+class Solution {
+public:
+    int missingNumberOptimalApproachI(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<bool> track(nums.size() + 1, false);
+
+        for(int i = 0; i < n; i++){
+            track[nums[i]] = true;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(!track[i]){
+                return i;
+            }
+        }
+
+        return nums.size();
+    }
+};
