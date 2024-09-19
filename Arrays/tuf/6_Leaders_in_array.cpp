63755f6ea90945c0c19355ded2e2d1cbc4bc5795
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-19 21:56:07 +0530

    Arrays : Leaders in an array - Optimal approach

---
+++ Arrays/tuf/6_Leaders_in_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+      
+      vector<int> result;
+      int n = nums.size();
+
+      int currentLeader = nums[n-1];
+
+      result.push_back(currentLeader);
+
+      for(int i = n - 2; i >= 0; i--){
+
+          if(nums[i] > currentLeader){
+            result.push_back(nums[i]);
+            currentLeader = nums[i];
+          }
+      }
+
+      reverse(result.begin(), result.end());
+
+      return result;
+    }
+};
