2be6ce29dc0d5da421f4177c9f4c5d56837a00d5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-27 08:36:08 +0530

    Arrays : Pascal's Triangle - Variety 1 (using nCr formula)

--- Arrays/tuf/9_Pascals_triangle.cpp
+++ Arrays/tuf/9_Pascals_triangle.cpp
@@ -3,7 +3,7 @@
 #include<algorithm>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> pascalTriangle(int numRows) {
         
@@ -29,5 +29,37 @@ public:
         }
 
         return ans;
+    }
+};
+
+
+// variety 1 : (Creating Pascal's triangle by combination formula (nCr) , to calculate current value by n = row vale and c = col value )
+class Solution2 {
+public:
+    vector<vector<int>> pascalTriangle(int numRows) {
+        
+        vector<vector<int>> ans(numRows);
+
+        for(int i = 0; i < numRows; i++){
+            
+            ans[i].resize(i+1);
+
+            for(int j = 0; j <= i; j++){
+                ans[i][j] = nCr(i, j);
+            }
+        }
+
+        return ans;
+    }
+
+    int nCr(int n, int r){
+        long long res = 1;
+
+        for(int i = 0; i < r; i++){
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+
+        return (int)res;
     }
 };
