09854f8ff9de174a5206de7879e466128c41f95b
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-28 12:27:42 +0530

    Arrays : Pascal's Triangle - Rotate matrix by 90 degrees

---
+++ Arrays/tuf/10_Rotate_matrix_by_90_deg.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void rotateMatrix(vector<vector<int>>& matrix) {
+        
+        transPose(matrix);
+
+        reverseMatrix(matrix);
+    }
+
+    void transPose(vector<vector<int>>& matrix){
+
+        for(int i = 0; i < matrix.size(); i++){
+
+            for(int j = i; j < matrix[i].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
+                // int t =  matrix[i][j];
+                // matrix[i][j] = matrix[j][i];
+                // matrix[j][i] = t;
+            }
+        }
+    }
+
+    void reverseMatrix(vector<vector<int>>& matrix){
+        int i = 0;
+        int j = matrix[0].size() - 1;
+
+        while(i < j){
+
+            for(int k = 0; k < matrix.size(); k++){
+                swap(matrix[k][i], matrix[k][j]);
+            }
+
+            i++;
+            j--;
+        }
+    }
+};
