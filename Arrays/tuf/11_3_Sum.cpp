e0cb326a481788f01d2a7629054830e14da527a5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-30 21:25:38 +0530

    Arrays : 3 sum : Optimal Approach

--- Arrays/tuf/11_3_Sum.cpp
+++ Arrays/tuf/11_3_Sum.cpp
@@ -40,7 +40,7 @@ public:
 
 
 
-// Optimal Approach
+// Better Approach
 class Solution2 {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -73,5 +73,57 @@ public:
 
        return ans;
         
+    }
+};
+
+
+
+// Optimal Approach
+class Solution3 {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+       int n = nums.size();
+
+       vector<vector<int>> ans;
+
+       sort(nums.begin(), nums.end());
+
+       for(int i = 0; i < n; i++){
+
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int j = i + 1, k = n - 1;
+
+            while(j < k){
+
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum == 0){
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+
+                }
+                else if(sum < 0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+       }
+
+       return ans;
     }
 };
