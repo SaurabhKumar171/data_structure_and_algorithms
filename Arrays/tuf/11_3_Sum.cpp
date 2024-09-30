c73254be457585890f961aee13d97f10b0725d67
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-09-30 21:04:07 +0530

    Arrays : 3 sum : Better Approach

--- Arrays/tuf/11_3_Sum.cpp
+++ Arrays/tuf/11_3_Sum.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // Brute force Approach
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> threeSumBruteForceApproach(vector<int>& nums) {
         int n = nums.size();
@@ -34,6 +34,44 @@ public:
         vector<vector<int>> ans(tripletSet.begin(), tripletSet.end());
 
         return ans;
+        
+    }
+};
+
+
+
+// Optimal Approach
+class Solution2 {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n = nums.size();
+
+       set<vector<int>> tripletSet;
+
+       for(int i = 0; i < n; i++){
+
+            set<int> hashset;
+
+            for(int j = i + 1; j < n; j++){
+
+                int third = -(nums[i] + nums[j]);
+
+                if(hashset.find(third) != hashset.end()){
+                    vector<int> temp = {nums[i], nums[j], third};
+                    
+                    sort(temp.begin(), temp.end());
+                    tripletSet.insert(temp);
+                }
+
+                hashset.insert(nums[j]);
+            }
+
+       }
+
+       vector<vector<int>> ans(tripletSet.begin(), tripletSet.end());
+
+       return ans;
         
     }
 };
