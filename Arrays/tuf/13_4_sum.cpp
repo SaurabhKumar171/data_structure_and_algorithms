af0c1badc87c7db6e95eb400517ba0167dec9cf9
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-02 19:38:44 +0530

    Arrays : 4 sum : Optimal Approach

--- Arrays/tuf/13_4_sum.cpp
+++ Arrays/tuf/13_4_sum.cpp
@@ -39,3 +39,56 @@ public:
         return ans;
     }
 };
+
+
+// Optimal Approach
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++){
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for(int j = i+1; j < n - 2; j++){
+
+                if (j > i+1 && nums[j] == nums[j - 1])
+                    continue;
+                
+                int k = j + 1, p = n - 1;
+
+                while(k < p){
+
+                    long long sum = (long long)(nums[i])+(long long)(nums[j]) + (long long)(nums[k]) + (long long)(nums[p]);
+
+                    if(sum == (long long)target){
+                        vector<int> temp = {nums[i] , nums[j] , nums[k] , nums[p]};
+                        ans.push_back(temp);
+
+                        k++;
+                        p--;
+
+                        while(k < p && nums[k] == nums[k - 1]) k++;
+                        while(k < p && nums[p] == nums[p + 1]) p--;
+                    }
+                    else if(sum < target){
+                        k++;
+                    }
+                    else{
+                        p--;
+                    }
+                }
+
+            }
+        }
+
+        return ans;
+    }
+};
