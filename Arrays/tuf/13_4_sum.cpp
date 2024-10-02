e1d3a304e5e260adf82e7753091a6b61aa5a9c75
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-02 18:30:56 +0530

    Arrays : 4 sum : Brute force Approach

---
+++ Arrays/tuf/13_4_sum.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+
+        set<vector<int>> quadrupletsSet;
+
+        for(int i = 0; i < n - 3; i++){
+
+            for(int j = i + 1; j < n - 2; j++){
+
+                for(int k = j + 1; k < n - 1; k++){
+
+                    for(int p = k + 1; p < n; p++){
+                        
+                        long long sum = (long long)(nums[i])+(long long)(nums[j]) + (long long)(nums[k]) + (long long)(nums[p]);
+
+                        if(sum == (long long)(target)){
+                            vector<int> temp = {nums[i] , nums[j] , nums[k] , nums[p]};
+                            sort(temp.begin(), temp.end());
+
+                            quadrupletsSet.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>> ans(quadrupletsSet.begin(), quadrupletsSet.end());
+
+        return ans;
+    }
+};
