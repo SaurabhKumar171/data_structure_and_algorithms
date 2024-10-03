275895a72eafe527f4440aac21812a560a0a130f
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-03 21:24:11 +0530

    Arrays : Sort 0s, 1s, 2s : Better Approach

---
+++ Arrays/tuf/14_Sort_0_1_2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+// Better Approach
+class Solution {
+public:
+    void sortZeroOneTwo(vector<int>& a) {
+        
+        int zerCnt = 0, oneCnt = 0, twoCnt = 0;
+        int n = a.size();
+
+        for(int i = 0; i < n; i++){
+            if(a[i] == 0) zerCnt++;
+            else if(a[i] == 1) oneCnt++;
+            else twoCnt++;
+        }
+
+        int k = 0;
+        while(zerCnt > 0){
+            a[k++] = 0;
+            zerCnt--;
+        }
+        while(oneCnt > 0){
+            a[k++] = 1;
+            oneCnt--;
+        }
+        while(twoCnt > 0){
+            a[k++] = 2;
+            twoCnt--;
+        }
+    }
+};
