40dada9d8a8a69a24ca17c4275ab632c30ef5601
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-04 19:36:25 +0530

    Arrays : Majority element - II : Better Approach

---
+++ Arrays/tuf/16_Majority_element-II.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+// Brute force Approach
+class Solution1 {
+public:
+    vector<int> majorityElementTwo(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int k = n/3;
+
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++){
+
+            int cnt = 0;
+
+            if(ans.size() != 0 && ans[0] == nums[i]) continue;
+
+            for(int j = i; j < n; j++){
+
+                if(nums[i] == nums[j]){
+                    cnt++;
+                }
+            }
+
+            if(cnt > k){
+                ans.push_back(nums[i]);
+            }
+
+            if(ans.size() == 2){
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
