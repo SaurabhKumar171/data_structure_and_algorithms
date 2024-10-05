de10326ddc6ddee53bc21e1db9d546e80c7e6c4c
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-05 14:35:41 +0530

    Arrays : Find the repeating and missing number : Optimal Approach

--- Arrays/tuf/17_Find_the_repeating_and_missing_number.cpp
+++ Arrays/tuf/17_Find_the_repeating_and_missing_number.cpp
@@ -46,7 +46,7 @@ public:
 
 
 // Better approach
-class Solution {
+class Solution2 {
 public:
     vector<int> findMissingRepeatingNumbers(vector<int> nums) {
 
@@ -75,5 +75,36 @@ public:
         }
 
         return {repeatingNumber, missingNumber};
+    }
+};
+
+
+// Optimal Approach - I
+class Solution3 {
+public:
+    vector<int> findMissingRepeatingNumbers(vector<int> nums) {
+
+        long long n = nums.size();
+
+        long long s = 0, sn = 0;
+        long long s2 = 0, s2n = 0;
+
+        sn = (n*(n+1))/2;
+        s2n = (n*(n+1)*((2*n)+1))/6;
+
+        for(int i = 0; i < n; i++){
+
+            s += nums[i];
+
+            s2 += (long long)(nums[i])*(long long)(nums[i]); 
+        }
+
+        long long val1 = s - sn;
+        long long val2 = (s2 - s2n)/val1;
+
+        long long x = (val2 + val1)/2;
+        long long y = val2 - x;
+ 
+        return {(int)x , (int)y};
     }
 };
