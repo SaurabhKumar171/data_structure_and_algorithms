50e857f50aa00f0e8468b2c38a32a45bca52701c
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-08 15:25:59 +0530

    Arrays : Merge two sorted arrays without extra space - Bute Force Approach

---
+++ Arrays/tuf/19_Merge_two_sorted_arrays_without_extra_space.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+// Brute force
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        vector<int> temp;
+
+        int i = 0;
+        int j = 0;
+
+        while(i< m && j < n){
+
+            if(nums1[i] < nums2[j]){
+                temp.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                temp.push_back(nums2[j]);
+                j++;
+            }
+            else{
+                temp.push_back(nums1[i]);
+                temp.push_back(nums2[j]);
+                i++;
+                j++;
+            }
+        }
+
+        while(i < m){
+            temp.push_back(nums1[i]);
+            i++;
+        }
+
+        while(j < n){
+            temp.push_back(nums2[j]);
+            j++;
+        }
+
+        for(int i = 0; i < m+n; i++){
+            nums1[i] = temp[i];
+        }
+    }
+};
