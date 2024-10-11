01bee933d03d55483b492fb43d41b21550b30ea2
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-11 11:30:29 +0530

    Arrays : Merge two sorted arrays without extra space - Optimal approach - I

--- Arrays/tuf/19_Merge_two_sorted_arrays_without_extra_space.cpp
+++ Arrays/tuf/19_Merge_two_sorted_arrays_without_extra_space.cpp
@@ -84,3 +84,35 @@ public:
         }
     }
 };
+
+
+// Optimal approach - I
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i = m-1;
+        int j = 0;
+
+        while(i >= 0 && j < n){
+
+            if(nums1[i] > nums2[j]){
+                swap(nums1[i], nums2[j]);
+                i--;
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+
+        sort(nums1.begin(), nums1.begin() + m);
+
+        sort(nums2.begin(), nums2.end());
+
+        for(int i = m; i < m+n; i++){
+            nums1[i] = nums2[i-m];
+        }
+
+    }
+};
