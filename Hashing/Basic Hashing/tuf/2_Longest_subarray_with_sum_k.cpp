99599faad17b1bd5c4c6b4680fbbbbdf3acaa2d0
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-14 20:04:05 +0530

    Hashing : Subarray Sum Equals K

---
+++ Hashing/Basic Hashing/tuf/2_Longest_subarray_with_sum_k.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+// Brute force Approach
+class Solution1{
+public:
+    int longestSubarray(vector<int> &nums, int k){
+        
+        int n = nums.size();
+        int longestSubarray = 0;
+
+        for(int i = 0; i < n; i++){
+
+            int cnt = 0;
+            int sum = 0;
+
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+
+                if(sum == k){
+                    cnt = j-i+1;
+                }
+                else{
+                    continue;
+                }
+            }
+
+            longestSubarray = max(longestSubarray, cnt);
+        }
+
+        return longestSubarray;
+    }
+};
---
+++ Hashing/Basic Hashing/tuf/3_Subarray_sum_equals_k.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+// Brute force Approach
+class Solution1 {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        int subarrayCnt = 0;
+
+        for(int i = 0; i < n; i++){  
+
+            int cnt = 0;
+            int sum = 0;
+
+            for(int j = i; j < n; j++){  
+                sum += nums[j];
+
+                if(sum == k){
+                    subarrayCnt++;
+                }
+            }
+        }
+
+        return subarrayCnt;
+    }
+};
