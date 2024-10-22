dbb0a4b739d4e0257a163c4edd075be39118b299
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-22 17:22:05 +0530

    Hashing : Count subarray with sum equals k

--- Hashing/Basic Hashing/tuf/3_Count_subarray_sum_equals_k.cpp
+++ Hashing/Basic Hashing/tuf/3_Count_subarray_sum_equals_k.cpp
@@ -10,22 +10,22 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         
         int n = nums.size();
-        int subarrayCnt = 0;
+        int longestSubarray = 0;
+        int cnt = 0;
 
-        for(int i = 0; i < n; i++){  
+        for(int i = 0; i < n; i++){
 
-            int cnt = 0;
             int sum = 0;
 
-            for(int j = i; j < n; j++){  
+            for(int j = i; j < n; j++){
                 sum += nums[j];
 
                 if(sum == k){
-                    subarrayCnt++;
-                }   
+                    cnt++;
+                }
             }
         }
-     
-        return subarrayCnt;
+
+        return cnt;
     }
 };
