4a7c94023cc21ef9ac2fcbe0a624720afb31c372
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-10-24 22:03:47 +0530

    Binary search : Search in Rotated Sorted Array

--- Binary Search/Logic Building/4_search_in_rotated_sorted_array.cpp
+++ Binary Search/Logic Building/4_search_in_rotated_sorted_array.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int k) {
-        
+    int search(vector<int> &nums, int k) {
+       
        int low = 0;
        int high = nums.size() - 1;
 
@@ -16,12 +16,14 @@ public:
         int mid = low + (high - low)/2;
 
         if(nums[mid] == k) return mid;
-
+        
+        // Check if the left part is sorted
         if(nums[mid] >= nums[low]){
             if(k >= nums[low] && k <= nums[mid]) high = mid - 1;
             else low = mid + 1;
         }
         else{
+            // Check if the right part is sorted
             if(k >= nums[mid] && k <= nums[high]) low = mid + 1;
             else high = mid - 1;
         }
