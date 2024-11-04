dc6e838ad7f47a97e6b82b8eae8537de6de93e59
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-11-04 22:25:55 +0530

    Binary search : Minimum days to make M bouquets

--- Binary Search/On Answers/5_minimum_number_of_days_to_make_m_bouquets.cpp
+++ Binary Search/On Answers/5_minimum_number_of_days_to_make_m_bouquets.cpp
@@ -47,3 +47,57 @@ public:
    return -1;
   }
 };
+
+
+// Brute force Approach ( Binary search )
+class Solution {
+private:
+    bool possible(vector<int> &nums, int day, int m, int k) {
+        int n = nums.size(); 
+        int cnt = 0;
+        int noOfB = 0;
+
+        for(int i = 0; i< n; i++){
+          if(nums[i] <= day){
+            cnt++;
+          }
+          else{
+            noOfB += (cnt/k);
+            cnt = 0;
+          }
+        }
+        noOfB += (cnt / k); 
+
+        return noOfB >= m;
+    }
+
+public:
+    int minDays(vector<int>& nums, int m, int k) {
+        
+        int n = nums.size(); 
+        long long val = m * 1ll * k * 1ll; 
+            
+        if (val > n) return -1; 
+        
+        int mini = INT_MAX, maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, nums[i]); 
+            maxi = max(maxi, nums[i]); 
+        }
+
+        int left = mini, right = maxi, ans = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(possible(nums, mid, m, k)){
+                ans = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
