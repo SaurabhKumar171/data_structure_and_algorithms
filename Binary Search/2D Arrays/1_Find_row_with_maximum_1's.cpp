baf12ca7113aeedf555299f31eba48600466e410
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-11-10 19:13:10 +0530

    Binary search : Find row with maximum 1's

--- Binary Search/2D Arrays/1_Find_row_with_maximum_1's.cpp
+++ Binary Search/2D Arrays/1_Find_row_with_maximum_1's.cpp
@@ -31,3 +31,40 @@ class Solution {
     return maxOneRow;
   }
 };
+
+
+// Optimal approach [O( m * log(n) )]
+class Solution {
+  public:   
+  int rowWithMax1s(vector < vector < int >> & mat) {
+    
+
+    int maxOneRow = -1;
+    int maxOnes = 0;
+    for(int i = 0; i < mat.size(); i++){
+
+      int countOne = 0;
+      
+      int l = 0, h = mat[i].size()-1;
+
+      while(l <= h){
+        int m = (l+h)/2;
+
+        if(mat[i][m] == 0){
+          l = m + 1;
+        }
+        else{
+          countOne = mat[i].size() - m +1 ;
+          h = m - 1;
+        }
+      }
+
+      if(countOne > maxOnes){
+        maxOnes = countOne;
+        maxOneRow = i;
+      }
+    }
+
+    return maxOneRow;
+  }
+};
