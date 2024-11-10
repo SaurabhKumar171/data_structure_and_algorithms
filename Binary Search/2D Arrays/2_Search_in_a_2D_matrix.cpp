646cdecfc281b486184c0bdbfff06e1ffc11266e
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-11-10 19:59:39 +0530

    Binary search : Search in a 2D matrix

--- Binary Search/2D Arrays/2_Search_in_a_2D_matrix.cpp
+++ Binary Search/2D Arrays/2_Search_in_a_2D_matrix.cpp
@@ -4,7 +4,33 @@
 #include<set>
 using namespace std;
 
-// Optimal approach [O( m * log(n) )]
+// Optimal approach [O(log(n) )]
+class Solution{
+public:
+    bool searchMatrix(vector<vector<int>> &mat, int target){
+
+        int cols = mat[0].size();
+        int l = 0;
+        int h = (mat.size()*mat[0].size()) - 1;
+
+        while(l <= h){
+
+            int m = l + (h-l)/2;
+
+            int r = m/cols;
+            int c = m%cols;
+
+            if(mat[r][c] == target) return true;
+            else if(mat[r][c] < target) l = m + 1;
+            else h = m - 1;
+        }
+
+        return false;
+    }
+};
+
+
+// Better approach [O( n * log(n) )]
 class Solution{
 private:
     bool binarySearch(vector<int> a, int x){
