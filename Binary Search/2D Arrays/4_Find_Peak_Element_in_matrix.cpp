44269b701321129743152e8ae2c7cecad42207cc
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-11-13 19:42:02 +0530

    Binar search : Peak element in matrix

--- Binary Search/2D Arrays/4_Find_Peak_Element_in_matrix.cpp
+++ Binary Search/2D Arrays/4_Find_Peak_Element_in_matrix.cpp
@@ -4,6 +4,57 @@
 #include<set>
 using namespace std;
 
+
+// Optimal Approach (r * log(c))
+class Solution {
+private:
+    int max_ele(vector<vector<int>>& mat, int col){
+
+        int maxEle = mat[0][col];
+        int maxId = 0;
+
+        for(int i = 0; i < mat.size(); i++){
+            if(mat[i][col] >= maxEle){
+
+                maxEle = mat[i][col];
+                maxId = i;
+            }
+        }
+        return maxId;
+    }
+
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int r = mat.size();
+        int c = mat[0].size();
+
+        int l = 0;
+        int h = c - 1;
+
+        while(l <= h){
+            int m = (l + h)/2;
+
+            int maxRowId = max_ele(mat, m);
+
+            int left = (m > 0) ? mat[maxRowId][m-1] : INT_MIN;
+            int right = (m < c -1) ? mat[maxRowId][m+1] : INT_MIN;
+
+            if(mat[maxRowId][m] > left && mat[maxRowId][m] > right){
+                return {maxRowId, m};
+            }
+            else if(mat[maxRowId][m] < right){
+                l = m + 1;
+            }
+            else{
+                h = m - 1;
+            }
+        }
+
+        return {-1 , -1};     
+    }
+};
+
+
 // Brute force [O(n^2)]
 class Solution {
 public:
