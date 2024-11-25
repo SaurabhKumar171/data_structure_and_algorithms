803c51da2f10719e5c096c29807ed6e3dd7b38fd
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-11-25 14:03:55 +0530

    Recursion : Pow(x,n)

---
+++ Recursion/Implementation_Problems/1_pow(x,n).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 1){
+            return x;
+        }
+        else if(n == 0){
+            return 1;
+        }
+
+        long long num = n;
+        if(n < 0){
+            num = -1 * num;
+            x = 1/x;
+        }
+
+        if(n % 2 == 0){
+            return myPow(x*x, (int)(num/2));
+        }
+        else{
+            return x*myPow(x, (int)(num-1));
+        }
+
+        return -1;  
+    }
+};
+
