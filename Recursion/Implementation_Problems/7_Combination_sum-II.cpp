ab637ef4ab7064115d95a7db9184cdb6edfd9e0a
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-05 10:52:07 +0530

    Recursion : Combination Sum - II

---
+++ Recursion/Implementation_Problems/7_Combination_sum-II.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include <numeric>
+using namespace std;
+
+class Solution {
+private:
+    void func(int index, vector<int>& list, int sum, vector<vector<int>>& ans, vector<int>& candidates){
+
+        if(sum == 0){
+            ans.push_back(list);
+            return;
+        }
+
+        if(sum < 0 || index == candidates.size()) return;
+
+        list.push_back(candidates[index]);
+
+        func(index + 1, list, sum - candidates[index], ans, candidates);
+
+        list.pop_back();
+
+        for(int j = index + 1; j < candidates.size(); j++){
+            if(candidates[j] != candidates[index]){
+                func(j, list, sum, ans, candidates);
+                break;
+            }
+        }
+
+        // func(index + 1, list, sum, ans, candidates);
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        sort(candidates.begin(), candidates.end());
+
+        vector<int> list;
+        vector<vector<int>> ans;
+        
+        func(0, list, target, ans, candidates);
+
+        return ans;
+    }
+};
