3c0b4bb850479219f617360c953ffcbe2e97d3a1
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-09 12:10:31 +0530

    Recursion : Letter Combinations of a Phone Number

--- Recursion/Implementation_Problems/11_Letter_combinations_of_a_phone_number.cpp
+++ Recursion/Implementation_Problems/11_Letter_combinations_of_a_phone_number.cpp
@@ -5,3 +5,40 @@
 #include <set>
 #include <numeric>
 using namespace std;
+
+class Solution
+{
+private:
+    void func(int index, vector<string> &combos, vector<string> &ans, string digits, string s)
+    {
+
+        if (digits.size() == 0)
+            return;
+
+        if (index == digits.size())
+        {
+            ans.push_back(s);
+            return;
+        }
+
+        int number = digits[index] - '0';
+
+        for (int i = 0; i < combos[number].size(); i++)
+        {
+            func(index + 1, combos, ans, digits, s + combos[number][i]);
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        vector<string> combos = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        vector<string> ans;
+        string s;
+
+        func(0, combos, ans, digits, s);
+
+        return ans;
+    }
+};
