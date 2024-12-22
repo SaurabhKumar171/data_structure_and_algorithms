f0ee44a8852b4a36de70cbea7ac2d690d0f86829
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-22 22:55:45 +0530

    Linked List : Odd Even Linked List (Optimal solution)

--- Linked Lists/Basics of Linked Lists/22_Insert_before_given_node_in_DLL.cpp
+++ Linked Lists/Basics of Linked Lists/22_Insert_before_given_node_in_DLL.cpp
@@ -1,6 +1,6 @@
 
 
-// Definition of doubly linked list:
+// Brute force
 struct ListNode
 {
     int val;
@@ -40,5 +40,43 @@ public:
         ListNode *newNode = new ListNode(X, temp, prevNode);
         prevNode->next = newNode;
         temp->prev = newNode;
+    }
+};
+
+// Optimal Solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *firstEven = head->next;
+
+        while (even != NULL && even->next != NULL)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = firstEven;
+
+        return head;
     }
 };
