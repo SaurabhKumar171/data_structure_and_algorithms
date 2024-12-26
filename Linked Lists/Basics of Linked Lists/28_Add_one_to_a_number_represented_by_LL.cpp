f7bcd1bf6ed853b9f488698f7c7985d10d68b2b5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-26 22:53:27 +0530

    Linked List : Reverse a LL - Brute force - Add one to a number represented by LL

--- Linked Lists/Basics of Linked Lists/28_Add_one_to_a_number_represented_by_LL.cpp
+++ Linked Lists/Basics of Linked Lists/28_Add_one_to_a_number_represented_by_LL.cpp
@@ -1,5 +1,5 @@
 
-// Brute force [T.C - 0(n), S.C - 0(n)]
+// Brute force [T.C - 0(n), S.C - 0(1)]
 class Solution
 {
 private:
@@ -54,5 +54,44 @@ public:
         newHead = reverseLL(dummyNode);
 
         return newHead;
+    }
+};
+
+// Optimal solution [T.C - 0(n), S.C - 0(n) : due to recursion call stack]
+class Solution
+{
+private:
+    int helper(Node *temp)
+    {
+        if (temp == nullptr)
+            return 1;
+
+        int carry = helper(temp->next);
+
+        temp->data = temp->data + carry;
+
+        if (temp->data < 10)
+        {
+            return 0;
+        }
+
+        temp->data = temp->data % 10;
+
+        return 1;
+    }
+
+public:
+    Node *addOne(Node *head)
+    {
+        int carry = helper(head);
+
+        if (carry == 1)
+        {
+            Node *newNode = new Node(carry);
+            newNode->next = head;
+            return newNode;
+        }
+
+        return head;
     }
 };
