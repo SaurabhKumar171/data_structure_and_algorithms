562205bc33ea5305bdd2ae1a5db9133f898d1937
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-26 14:31:33 +0530

    Linked List : Reverse a LL  - Brute force solution

--- Linked Lists/Basics of Linked Lists/26_ Remove_Nth_Node_From_End_of_List.cpp
+++ Linked Lists/Basics of Linked Lists/26_ Remove_Nth_Node_From_End_of_List.cpp
@@ -48,3 +48,36 @@ public:
         return head;
     }
 };
+
+// Optimal solution
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast->next;
+        }
+
+        if (fast == NULL)
+        {
+            return head->next;
+        }
+
+        while (fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        ListNode *delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
+        return head;
+    }
+};
