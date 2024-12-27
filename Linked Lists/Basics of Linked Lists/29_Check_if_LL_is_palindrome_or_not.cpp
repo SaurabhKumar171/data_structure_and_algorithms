c90a98d3f745912e06b823f11625a2952f4a42c5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-27 13:25:58 +0530

    Linked List : Reverse a LL - Brute force - Check if LL is palindrome or not (optimal soln [T.C - O(n), S.C - O(1)])

--- Linked Lists/Basics of Linked Lists/29_Check_if_LL_is_palindrome_or_not.cpp
+++ Linked Lists/Basics of Linked Lists/29_Check_if_LL_is_palindrome_or_not.cpp
@@ -1,3 +1,4 @@
+// Brute force
 class Solution
 {
 public:
@@ -23,6 +24,88 @@ public:
             }
             st.pop();
             curr = curr->next;
+        }
+
+        return true;
+    }
+};
+
+// optimal soln [T.C - O(n), S.C - O(1)]
+
+class Solution
+{
+private:
+    int sizeOfLL(ListNode *head)
+    {
+        ListNode *curr = head;
+        int cnt = 0;
+
+        while (curr != NULL)
+        {
+            cnt++;
+            curr = curr->next;
+        }
+
+        return cnt;
+    }
+
+    ListNode *middleNode(ListNode *head)
+    {
+        int size = sizeOfLL(head);
+
+        int mid = 0;
+        if (size % 2 != 0)
+        {
+            mid = (size + 1) / 2;
+        }
+        else
+        {
+            mid = (size / 2) + 1;
+        }
+
+        ListNode *curr = head;
+        while (mid > 1)
+        {
+            mid--;
+            curr = curr->next;
+        }
+
+        return curr;
+    }
+
+    ListNode *reverse(ListNode *head)
+    {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+
+        while (curr != nullptr)
+        {
+            ListNode *nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode *head)
+    {
+
+        ListNode *midNode = middleNode(head);
+        ListNode *reversedHead = reverse(midNode);
+
+        ListNode *curr = head;
+        while (reversedHead != nullptr)
+        {
+            if (curr->val != reversedHead->val)
+            {
+                return false;
+            }
+
+            curr = curr->next;
+            reversedHead = reversedHead->next;
         }
 
         return true;
