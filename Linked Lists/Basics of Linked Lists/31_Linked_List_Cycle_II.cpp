bc7a26db5557a41c8a0c8f5d34d6c34a520a0492
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-29 14:21:25 +0530

    Linked List : Linked List Cycle II

--- Linked Lists/Basics of Linked Lists/31_Linked_List_Cycle_II.cpp
+++ Linked Lists/Basics of Linked Lists/31_Linked_List_Cycle_II.cpp
@@ -25,3 +25,39 @@ public:
         return nullptr;
     }
 };
+
+// Optimal Solution
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        bool loopExists = false;
+
+        while (fast != nullptr && fast->next != nullptr)
+        {
+
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow)
+            {
+                slow = head;
+
+                while (slow != fast)
+                {
+
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+
+                return slow;
+            }
+        }
+
+        return nullptr;
+    }
+};
