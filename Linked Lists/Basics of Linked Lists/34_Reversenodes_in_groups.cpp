6b1a82358e656f908737f82e17a25183ff7fefb2
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-02 11:42:16 +0530

    Linked List : Reverse Nodes in k-Group

---
+++ Linked Lists/Basics of Linked Lists/34_Reversenodes_in_groups.cpp
@@ -0,0 +1,72 @@
+// Brute force (T.C - O(2n), S.C - O(1))
+
+class Solution
+{
+private:
+    ListNode *getKthNode(ListNode *curr, int k)
+    {
+        k -= 1;
+        while (k > 0 && curr != nullptr)
+        {
+            k--;
+            curr = curr->next;
+        }
+
+        return curr;
+    }
+
+    ListNode *reverseLL(ListNode *head)
+    {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+
+        while (curr != nullptr)
+        {
+            ListNode *nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+public:
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        ListNode *temp = head;
+        ListNode *prevNode = nullptr;
+
+        while (temp != nullptr)
+        {
+            ListNode *kThNode = getKthNode(temp, k);
+
+            if (kThNode == nullptr)
+            {
+                if (prevNode)
+                    prevNode->next = temp;
+
+                break;
+            }
+
+            ListNode *nextNode = kThNode->next;
+            kThNode->next = nullptr;
+
+            reverseLL(temp);
+
+            if (temp == head)
+            {
+                head = kThNode;
+            }
+            else
+            {
+                prevNode->next = kThNode;
+            }
+
+            prevNode = temp;
+            temp = nextNode;
+        }
+
+        return head;
+    }
+};
