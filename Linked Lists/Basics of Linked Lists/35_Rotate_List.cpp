b6e803259526d50ce9e3780028221cd350df0f02
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-02 12:52:19 +0530

    Linked List : Rotate List - Optimal soln

--- Linked Lists/Basics of Linked Lists/35_Rotate_List.cpp
+++ Linked Lists/Basics of Linked Lists/35_Rotate_List.cpp
@@ -8,6 +8,7 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+// Brute force (T.C - O(n + n/2), S.C - O(1))
 class Solution
 {
 private:
@@ -43,6 +44,77 @@ public:
         {
             newHead = rotateList(newHead);
         }
+
+        return newHead;
+    }
+};
+
+// Optimal approach
+class Solution
+{
+private:
+    ListNode *rotateList(ListNode *head, int pivot)
+    {
+
+        ListNode *curr = head;
+        ListNode *temp = head;
+        ListNode *prev = nullptr;
+
+        while (pivot && curr->next != nullptr)
+        {
+            pivot--;
+            prev = curr;
+            curr = curr->next;
+        }
+
+        ListNode *endNode = head;
+        while (endNode->next != nullptr)
+        {
+            endNode = endNode->next;
+        }
+
+        endNode->next = temp;
+        prev->next = nullptr;
+        head = curr;
+
+        return head;
+    }
+
+    int getLength(ListNode *head)
+    {
+        ListNode *curr = head;
+        int len = 0;
+        while (curr != nullptr)
+        {
+            len++;
+            curr = curr->next;
+        }
+
+        return len;
+    }
+
+public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+
+        int lengthOfLL = getLength(head);
+
+        if (head == nullptr || head->next == nullptr || (k % lengthOfLL) == 0)
+            return head;
+
+        int pivot = 0;
+
+        if (lengthOfLL < k)
+        {
+            pivot = lengthOfLL - (k % lengthOfLL);
+        }
+        else
+        {
+            pivot = lengthOfLL - k;
+        }
+
+        ListNode *newHead = head;
+        newHead = rotateList(newHead, pivot);
 
         return newHead;
     }
