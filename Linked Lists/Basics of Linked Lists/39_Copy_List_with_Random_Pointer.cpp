6ff87ca4c2b752859cb81e86e891382489fbf6bc
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-05 23:34:48 +0530

    Lisked List : Sort LL - Copy List with Random Pointer (Optimal soln)

--- Linked Lists/Basics of Linked Lists/39_Copy_List_with_Random_Pointer.cpp
+++ Linked Lists/Basics of Linked Lists/39_Copy_List_with_Random_Pointer.cpp
@@ -1,4 +1,4 @@
-// Brute force (T.C - {O(N)}, S.C -{O(N)})
+// Brute force (T.C - {O(N)}, S.C -{O(N) + O(N)})
 class Solution
 {
 public:
@@ -27,5 +27,52 @@ public:
         }
 
         return mp[head];
+    }
+};
+
+// Optimal Soln (T.C - { O(N)}, S.C - O(N))
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        if (!head)
+            return nullptr;
+
+        // Step 1: Create a copy of each node and insert it right after the original node.
+        Node *curr = head;
+        while (curr != nullptr)
+        {
+            Node *newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = newNode->next;
+        }
+
+        // Step 2: Assign random pointers for the copied nodes.
+        curr = head;
+        while (curr != nullptr)
+        {
+            if (curr->random != nullptr)
+            {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+
+        // Step 3: Separate the copied list from the original list.
+        curr = head;
+        Node *dummyNode = new Node(-1);
+        Node *copyCurr = dummyNode;
+
+        while (curr != nullptr)
+        {
+            copyCurr->next = curr->next;
+            curr->next = curr->next->next;
+            curr = curr->next;
+            copyCurr = copyCurr->next;
+        }
+
+        return dummyNode->next;
     }
 };
