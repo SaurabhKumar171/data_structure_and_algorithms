d873d42f84b9f2708a2a8c48ab39d3de5e6bcdee
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-05 21:03:24 +0530

    Lisked List : Sort LL - Optimal soln

--- Linked Lists/Basics of Linked Lists/38_Sort_LL.cpp
+++ Linked Lists/Basics of Linked Lists/38_Sort_LL.cpp
@@ -27,3 +27,78 @@ public:
         return dummyNode->next;
     }
 };
+
+// Optimal Soln (T.C - { O(nlong * (n+n/2))}, S.C - O(logn))
+class Solution
+{
+private:
+    ListNode *mergeLL(ListNode *left, ListNode *right)
+    {
+        ListNode *dummyNode = new ListNode(-1);
+        ListNode *curr = dummyNode;
+
+        while (left != nullptr && right != nullptr)
+        {
+            if (left->val <= right->val)
+            {
+                curr->next = left;
+                left = left->next;
+            }
+            else
+            {
+                curr->next = right;
+                right = right->next;
+            }
+
+            curr = curr->next;
+        }
+
+        while (left != nullptr)
+        {
+            curr->next = left;
+            left = left->next;
+            curr = curr->next;
+        }
+
+        while (right != nullptr)
+        {
+            curr->next = right;
+            right = right->next;
+            curr = curr->next;
+        }
+
+        return dummyNode->next;
+    }
+
+    ListNode *findMiddle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast->next != nullptr && fast->next->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+public:
+    ListNode *sortList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *mid = findMiddle(head);
+
+        ListNode *leftHead = head;
+        ListNode *rightHead = mid->next;
+        mid->next = nullptr;
+
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+
+        return mergeLL(leftHead, rightHead);
+    }
+};
