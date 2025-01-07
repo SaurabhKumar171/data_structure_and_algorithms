595f3ae11bd1b8a71c8405a99974fad47839bfc6
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-07 11:45:31 +0530

    Bit manipulation : K-th Bit is Set or Not

---
+++ Bit Manipulation/1_K-th_Bit_is_Set_or_Not.cpp
@@ -0,0 +1,27 @@
+// using left shift
+class Solution
+{
+public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+
+        return (n & (1 << k));
+    }
+};
+
+// using right shift
+class Solution
+{
+public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+
+        return ((n >> k) & 1);
+    }
+};
