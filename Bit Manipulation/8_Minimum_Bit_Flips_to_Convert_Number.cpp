f7d8f63681994256d673be8043089a9b03dbc182
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-08 22:58:39 +0530

    Bit Manipulation : Minimum Bit Flips to Convert Number

---
+++ Bit Manipulation/8_Minimum_Bit_Flips_to_Convert_Number.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int minBitsFlip(int start, int goal)
+    {
+        // XOR to find diff. bits
+        int diffBits = start ^ goal;
+
+        int cnt = 0;
+        // Count the number of 1s in diffBits (differing bits)
+        while (diffBits != 1)
+        {
+            cnt += diffBits & 1; // increment if last bit is 1
+            diffBits >>= 1;      // shift to process the next bit
+        }
+
+        if (diffBits == 1)
+            cnt++;
+
+        return cnt;
+    }
+};
