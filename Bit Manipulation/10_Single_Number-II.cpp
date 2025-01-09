2034e753b51ee4c4fb7ee150249be7df74997cd5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-09 21:50:44 +0530

    Bit Manipulation : Single Number - II

--- Bit Manipulation/10_Single_Number-II.cpp
+++ Bit Manipulation/10_Single_Number-II.cpp
@@ -1,7 +1,8 @@
+// Optimal Soln - I (T.C - { O(n*32)}, S.C - O(1))
 class Solution
 {
 public:
-    int singleNumber(vector<int> &nums)
+    int singleNumberII(vector<int> &nums)
     {
         int res = 0;
         for (int bitIndex = 0; bitIndex < 32; bitIndex++)
@@ -16,6 +17,38 @@ public:
 
             if (cnt % 3 == 1)
                 res = res | (1 << bitIndex);
+        }
+
+        return res;
+    }
+};
+
+// Optimal Soln - II (T.C - { O(nlogn)}, S.C - O(1))
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        // your code goes here
+        sort(nums.begin(), nums.end());
+
+        int res = nums[nums.size() - 1];
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+
+            if (nums[i - 1] != nums[i])
+            {
+                return nums[i - 1];
+            }
+            else if (nums[i + 1] != nums[i])
+            {
+                return nums[i + 1];
+            }
+            else
+            {
+                i += 2;
+            }
         }
 
         return res;
