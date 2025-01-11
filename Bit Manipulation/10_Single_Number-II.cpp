748e4bebeac873a306012258215337c37ba47472
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-01-11 11:16:57 +0530

    Bit Manipulation : Single Number II

--- Bit Manipulation/10_Single_Number-II.cpp
+++ Bit Manipulation/10_Single_Number-II.cpp
@@ -54,3 +54,22 @@ public:
         return res;
     }
 };
+
+// Optimal Soln - II (T.C - { O(n)}, S.C - O(1))
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ones = 0, twos = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+
+            ones = (ones ^ nums[i]) & (~twos);
+            twos = (twos ^ nums[i]) & (~ones);
+        }
+
+        return ones;
+    }
+};
