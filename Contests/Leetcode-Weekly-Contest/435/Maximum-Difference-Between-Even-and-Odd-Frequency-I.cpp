44c81fd7d85e24942ef5c9275067199b9da3b1d4
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-02-02 20:56:24 +0530

    Leetcode weekly contest : 435

---
+++ Contests/Leetcode-Weekly-Contest/435/Maximum-Difference-Between-Even-and-Odd-Frequency-I.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxDifference(string s)
+    {
+        unordered_map<char, int> mp;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            mp[s[i]]++;
+        };
+
+        int maxOdd = INT_MIN;
+        int minEven = INT_MAX;
+        for (auto i : mp)
+        {
+            if (i.second % 2 == 0)
+            {
+                minEven = min(minEven, i.second);
+            }
+            else
+            {
+                maxOdd = max(maxOdd, i.second);
+            }
+        }
+
+        return maxOdd - minEven;
+    }
+};
