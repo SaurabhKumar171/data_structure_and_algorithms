a0e9352e34993ede0a606684ea9afc5b0cdc4ab9
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-02-25 22:33:44 +0530

    Greedy Algorithms : Minimum number of platforms required for a railway

--- Contests/Leetcode-Weekly-Contest/435/Maximum-Difference-Between-Even-and-Odd-Frequency-I.cpp
+++ Contests/Leetcode-Weekly-Contest/435/Maximum-Difference-Between-Even-and-Odd-Frequency-I.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 class Solution{
     public:
-    int maxDifference(string s){
+    int maxDifference(string s)
+    {
         unordered_map<char, int> mp;
 
         for (int i = 0; i < s.size(); i++)
