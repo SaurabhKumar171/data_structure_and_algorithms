77f5821cf6f91011a03219c8154173c7d27cce42
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-04-16 21:13:22 +0530

    Subarray Sums Divisible by K

--- Hashing/contest/1_Subarray_Sums_Divisible_by_K.cpp
+++ Hashing/contest/1_Subarray_Sums_Divisible_by_K.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 class Solution {
 public:
-    int subarraysDivByK(vector<int>& nums, int k) {
+    int subarraysDivByK(vector<int>& nums, int k) 
+    {
         int sum = 0;
         int count = 0;
         unordered_map<int, int> prefixSumMap;
