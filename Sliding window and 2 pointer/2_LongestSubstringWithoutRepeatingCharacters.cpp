38ee3379d298bbf73b8a1aa78fb51a649a283720
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-05-22 17:29:15 +0530

    Longest Substring Without Repeating Characters

--- Sliding window and 2 pointer/2_LongestSubstringWithoutRepeatingCharacters.cpp
+++ Sliding window and 2 pointer/2_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -50,3 +50,26 @@ class Solution {
             return maxLen;
         }
     };
+
+
+// Better Approach: O(n) time complexity , O(n) space complexity
+    class Solution {
+        public:
+            int longestOnes(vector<int>& nums, int k) {
+                int maxLen = 0, n = nums.size(), l = 0, r = 0, zeroes = 0;
+        
+                while(r < n){
+                    if(nums[r] == 0) zeroes++;
+        
+                    if(zeroes > k){ 
+                        if(nums[l] == 0) zeroes--;
+                        l++;
+                    }
+        
+                    maxLen = max(maxLen, r - l + 1);    
+                    r++; 
+                }
+        
+                return maxLen;    
+            }
+        };
