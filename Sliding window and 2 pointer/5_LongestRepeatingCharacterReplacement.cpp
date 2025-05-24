495e0d7aaaed5e6838742fe4fb3ae5a49f9e723a
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-05-24 19:25:43 +0530

    Longest Repeating Character Replacement

--- Sliding window and 2 pointer/5_LongestRepeatingCharacterReplacement.cpp
+++ Sliding window and 2 pointer/5_LongestRepeatingCharacterReplacement.cpp
@@ -33,3 +33,38 @@ class Solution {
             return maxLen;        
         }
     };
+
+
+
+    // Better Approach: O(n * 26) time complexity , O(26) space complexity
+    class Solution {
+        public:
+            int characterReplacement(string s, int k) {
+                int n = s.size();
+                int l = 0, r = 0, maxFreq = 0, maxLen = 0;
+                vector<int> hash(26, 0);
+        
+        
+                while(r < n){
+                    hash[s[r] - 'A']++;
+        
+                    maxFreq = max(maxFreq, hash[s[r] - 'A']);
+        
+                    while((r - l + 1) - maxFreq > k){
+                        hash[s[l] - 'A']--;
+        
+                        maxFreq = 0;
+                        for(int i = 0; i < 26; i++){
+                            maxFreq = max(maxFreq, hash[i]);
+                        }
+        
+                        l++;
+                    }
+        
+                    maxLen = max(maxLen, r - l + 1);
+                    r++;
+                }
+        
+                return maxLen;        
+            }
+        };
