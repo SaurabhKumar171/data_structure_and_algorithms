4b18fa0598b80a70517f894fae05d945b327d8a5
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-05-26 23:22:28 +0530

    Next Greater Element

---
+++ Stacks/Medium/1_NextGreaterElement.cpp
@@ -0,0 +1,23 @@
+// Optimal Approach: O(n) time complexity, O(n) space complexity
+class Solution {
+    public:
+        vector<int> nextLargerElement(vector<int> arr) {
+            int n = arr.size();
+            vector<int> ans(n, -1);
+    
+            stack<int> st;
+    
+           for(int i = n - 1; i >= 0; i--){
+    
+            while(!st.empty() && st.top() <= arr[i]){
+                st.pop();
+            }
+    
+            if(!st.empty()) ans[i] = st.top();
+            st.push(arr[i]);
+           }
+    
+            return ans; 
+        }
+    };
+    
