0885d6d704b1f600edfe6de738e034f9cc833812
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-06-09 00:03:08 +0530

    Sum of Subarray Ranges

--- Stacks/Medium/5_SumofSubarrayRanges.cpp
+++ Stacks/Medium/5_SumofSubarrayRanges.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
 // Brute Force Approach : O(n^2) time complexity, O(1) space complexity
 class Solution {
     public:
@@ -20,5 +26,104 @@ class Solution {
             }
     
             return totalSum;
+        }
+    };
+
+
+
+// Optimal Approach : O(n) time complexity, O(n) space complexity
+class Solution {
+    private:
+        void pse(vector<int> &arr, vector<int> &pseArray) {
+            stack<int> st;
+            for (int i = 0; i < arr.size(); i++) {
+                while (!st.empty() && arr[i] < arr[st.top()]) {
+                    st.pop();
+                }
+                if (!st.empty()) pseArray[i] = st.top();
+                st.push(i);
+            }
+        }
+    
+        void nse(vector<int> &arr, vector<int> &nseArray) {
+            stack<int> st;
+            for (int i = arr.size() - 1; i >= 0; i--) {
+                while (!st.empty() && arr[i] <= arr[st.top()]) {
+                    st.pop();
+                }
+                if (!st.empty()) nseArray[i] = st.top();
+                st.push(i);
+            }
+        }
+    
+        long long sumSubarrayMins(vector<int> &arr) {
+            int n = arr.size();
+            int mod = 1e9 + 7;
+            long long ans = 0;
+    
+            vector<int> pseArray(n, -1);
+            vector<int> nseArray(n, n);
+    
+            pse(arr, pseArray);
+            nse(arr, nseArray);
+    
+            for (int i = 0; i < n; i++) {
+                long long left = i - pseArray[i];
+                long long right = nseArray[i] - i;
+                long long freq = left*right*1LL;
+                long long val = (freq*arr[i]*1LL);
+                ans += val;
+            }
+    
+            return ans;
+        }
+    
+        void pge(vector<int> &arr, vector<int> &pgeArray) {
+            stack<int> st;
+            for (int i = 0; i < arr.size(); i++) {
+                while (!st.empty() && arr[i] > arr[st.top()]) {
+                    st.pop();
+                }
+                if (!st.empty()) pgeArray[i] = st.top();
+                st.push(i);
+            }
+        }
+    
+        void nge(vector<int> &arr, vector<int> &ngeArray) {
+            stack<int> st;
+            for (int i = arr.size() - 1; i >= 0; i--) {
+                while (!st.empty() && arr[i] >= arr[st.top()]) {
+                    st.pop();
+                }
+                if (!st.empty()) ngeArray[i] = st.top();
+                st.push(i);
+            }
+        }
+    
+        long long sumSubarrayMaxs(vector<int> &arr) {
+            int n = arr.size();
+            int mod = 1e9 + 7;
+            long long ans = 0;
+    
+            vector<int> pgeArray(n, -1);
+            vector<int> ngeArray(n, n);
+    
+            pge(arr, pgeArray);
+            nge(arr, ngeArray);
+    
+            for (int i = 0; i < n; i++) {
+                long long left = i - pgeArray[i];
+                long long right = ngeArray[i] - i;
+                long long freq = left*right*1LL;
+                long long val = (freq*arr[i]*1LL);
+                ans += val;
+            }
+    
+            return ans;
+        }
+    public:
+        long long subArrayRanges(vector<int>& arr) {
+            return ( sumSubarrayMaxs(arr) - 
+                     sumSubarrayMins(arr) );
         }
     };
