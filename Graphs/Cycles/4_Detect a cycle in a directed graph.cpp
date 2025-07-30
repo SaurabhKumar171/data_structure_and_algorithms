8c8d7dbceaf4717f4a98aef086a41f7464ebca38
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-07-30 12:08:47 +0530

    Detect a cycle in a directed graph - DFS

---
+++ Graphs/Cycles/4_Detect a cycle in a directed graph.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Approach : O(V + E) time complexity ,  O(V + E) space complexity
+class Solution {
+   private:
+    bool dfs(int node, vector<int> adj[], vector<bool>& vis,
+             vector<bool>& pathVis) {
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                if (dfs(it, adj, vis, pathVis)) return true;
+            } else if (pathVis[it])
+                return true;
+        }
+
+        pathVis[node] = false;
+        return false;
+    }
+
+   public:
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<bool> vis(V, false);
+        vector<bool> pathVis(V, false);
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i])
+                if (dfs(i, adj, vis, pathVis)) return true;
+        }
+
+        return false;
+    }
+};
