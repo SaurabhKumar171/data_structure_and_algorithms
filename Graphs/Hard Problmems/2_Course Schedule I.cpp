55998d5aeaf810408700cde99968d060c763fe41
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-07-31 12:52:36 +0530

    Course Schedule I - BFS

--- Graphs/Hard Problmems/2_Course Schedule I.cpp
+++ Graphs/Hard Problmems/2_Course Schedule I.cpp
@@ -39,3 +39,53 @@ public:
         return true;
     }
 };
+
+
+//Topological sort or Kahn's algorithm
+//Approach : BFS - O(V + E) time complexity ,  O(V) space complexity
+class Solution{
+   private:
+    vector<int> topoSort(int V, vector<vector<int>> adj){
+        
+        vector<int> inDegree(V, 0);
+
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]) inDegree[it]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0) q.push(i);
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+
+            for(auto it: adj[node]){
+                inDegree[it]--;
+
+                if(inDegree[it] == 0) q.push(it);
+            }
+
+            ans.push_back(node);
+        }
+
+        return ans;
+    }
+public:
+    bool canFinish(int N, vector<vector<int>> arr) {
+        vector<vector<int>> adjLs(N);
+
+        for(int i = 0; i < arr.size(); i++){
+            adjLs[arr[i][0]].push_back(arr[i][1]);
+        }
+
+        vector<int> topo = topoSort(N, adjLs);
+
+        if(topo.size() < N) return false;
+
+        return true;
+    }
+};
