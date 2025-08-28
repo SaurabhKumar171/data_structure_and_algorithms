7da1e5f12c674200641cc686fc3a8b11bfad8610
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-08-28 12:31:07 +0530

    Bellma ford Algorithm

---
+++ Graphs/Shortest Path Algorithms/9_Bellman ford algorithm.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Adjacency list: node -> (neighbor, weight)
+        vector<int> distances(V, 1e9);
+        distances[S] = 0;
+        
+        for(int k = 1; k < V; k++){
+            for (auto &edge : edges) {
+                int u = edge[0], v = edge[1], w = edge[2];
+                
+                if(distances[u] != 1e9 && distances[v] > distances[u] + w){
+                    distances[v] = distances[u] + w;
+                }
+            }
+        }
+
+        // relaxation to check negative cycle
+        for (auto &edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            
+            if(distances[u] != 1e9 && distances[v] > distances[u] + w){
+                return {-1};
+            }
+        }
+
+        return distances;	
+	}
+};
