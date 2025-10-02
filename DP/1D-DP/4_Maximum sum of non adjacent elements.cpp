816f79a42e1f932cd3700ad6ec7c6241c86aca01
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-10-02 22:05:41 +0530

    Maximum sum of non adjacent elements

--- DP/1D-DP/4_Maximum sum of non adjacent elements.cpp
+++ DP/1D-DP/4_Maximum sum of non adjacent elements.cpp
@@ -25,3 +25,52 @@ public:
         return func(n - 1, n, nums, dp);
     }
 };
+
+
+// Tabulation
+// Time Complexity:O(n), where n is the number of elements in the array.
+// Space Complexity:O(n), due to the dp array.
+class Solution {
+public:
+    int nonAdjacent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+
+        dp[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            
+            int pick = nums[i];
+            if(i > 1) pick += dp[i - 2];
+
+            int notPick = 0 + dp[i - 1];
+            dp[i] = max(pick, notPick);
+        }
+
+        return dp[n - 1];
+    }
+};
+
+// Space Optimization
+// Time Complexity:O(n), where n is the number of elements in the array.
+// Space Complexity:O(1), as no extra space is used.
+class Solution {
+   public:
+    int nonAdjacent(vector<int>& nums) {
+        int n = nums.size();
+
+        int prev = nums[0];
+        int prev2 = 0;
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if (i > 1) pick += prev2;
+
+            int notPick = 0 + prev;
+
+            int cur_i = max(pick, notPick);
+            prev2 = prev;
+            prev = cur_i;
+        }
+
+        return prev;
+    }
+};
