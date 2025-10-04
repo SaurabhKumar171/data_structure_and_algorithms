05c34c950be3065c6fdc1618541b47c0d4170619
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-10-04 20:50:40 +0530

    Ninja's Training

--- DP/2D-DP/1_Ninja's training.cpp
+++ DP/2D-DP/1_Ninja's training.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 // Recursion + Memoization
+// Time Complexity:O(n), where n is the number of days due to the dynamic programming approach with memoization.
+// Space Complexity:O(n) due to the depth of the recursion stack and the dp table of size n x 4.
 class Solution {
    private:
     int func(int day, int last, vector<vector<int>>& matrix,
@@ -40,6 +42,8 @@ class Solution {
 
 
 // Tabulation
+// Time Complexity:O(n) due to the outer loop iterating 'n' times and the inner loops iterating a constant number of times.
+// Space Complexity:O(n) due to the usage of a 2D DP array of size n x 4.
 class Solution {
    public:
     int ninjaTraining(vector<vector<int>>& matrix) {
@@ -66,5 +70,39 @@ class Solution {
         }
 
         return dp[n - 1][3];
+    }
+};
+
+
+// Space Optimization
+// Time Complexity: O(N*4*3). There are N*4 states and for every state, we are running a for loop iterating three times.
+// Space Complexity:O(4), We are using an external array of size ‘4’ to store only one row.
+class Solution {
+   public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> prev(4, 0);
+
+        prev[0] = max(matrix[0][1], matrix[0][2]);
+        prev[1] = max(matrix[0][0], matrix[0][2]);
+        prev[2] = max(matrix[0][0], matrix[0][1]);
+        prev[3] = max(matrix[0][0], max(matrix[0][1], matrix[0][2]));
+
+        for (int day = 1; day < n; day++) {
+            vector<int> temp(4, 0);
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) {
+                        temp[last] =
+                            max(temp[last], matrix[day][task] + prev[task]);
+                    }
+                }
+            }
+            prev = temp;
+        }
+
+        return prev[3];
     }
 };
