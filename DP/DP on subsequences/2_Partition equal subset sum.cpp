a09d5b7148da0db9f03c278591d3afda7cd0bf7b
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-10-15 12:02:48 +0530

    Partition equal subset sum

--- DP/DP on subsequences/2_Partition equal subset sum.cpp
+++ DP/DP on subsequences/2_Partition equal subset sum.cpp
@@ -32,3 +32,75 @@ class Solution {
         return func(n - 1, arr, target, n, dp);
     }
 };
+
+
+
+
+
+// Tabulation
+class Solution {
+   public:
+    bool equalPartition(int n, vector<int> arr) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) sum += arr[i];
+
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
+
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        if (arr[0] <= target) dp[0][arr[0]] = true;
+
+        for (int id = 1; id < n; id++) {
+            for (int k = 1; k <= target; k++) {
+                bool pick = false;
+                bool notPick = false;
+                if (arr[id] <= k)
+                    pick = dp[id - 1][k - arr[id]];
+                notPick = dp[id - 1][k];
+
+                dp[id][k] = pick || notPick;
+            }
+        }
+
+        return dp[n - 1][target];
+    }
+};
+
+
+
+
+// Space Optimization
+class Solution {
+   public:
+    bool equalPartition(int n, vector<int> arr) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) sum += arr[i];
+
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+
+        vector<bool> prev(target + 1, 0), temp(target + 1, 0);
+
+        prev[0] = true;
+        if(arr[0] <= target) prev[arr[0]] = true;
+
+        for (int id = 1; id < n; id++) {
+            for (int k = 1; k <= target; k++) {
+                bool pick = false;
+                bool notPick = false;
+                if (arr[id] <= k)
+                    pick = prev[k - arr[id]];
+                notPick = prev[k];
+
+                temp[k] = pick || notPick;
+            }
+
+            prev = temp;
+        }
+
+        return prev[target];
+    }
+};
